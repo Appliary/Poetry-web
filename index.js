1f6f4ad0a6d9d3f776f798604317ee016300b98a
Michaël Carrette <miccarr@me.com>
2016-09-08 14:30:24 +0200

    API

--- index.js
+++ index.js
@@ -1,30 +1,14 @@
 const Log = require( 'poetry/lib/methods/log.js' ),
     Server = require( 'poetry/lib/server.js' ),
     Events = require( 'poetry/lib/methods/events.js' ),
-    http = require( 'http' );
+    http = require( 'http' ),
+    Joi = require( 'joi' ),
+    J2M = require( 'joi-to-markdown' );
 
 let routes = {};
 
 Server.register( [ {
     register: require( 'h2o2' )
-}, {
-    register: require( 'hapi-swaggered' ),
-    options: {
-        endpoint: '/api',
-        info: {
-            title: 'API Documentation',
-            version: Date.now()
-                .toString()
-        },
-        tagging: {
-            mode: 'tags'
-        },
-        requiredTags: [ 'all' ],
-        cors: {
-            origin: [ '*' ],
-            credentials: true
-        }
-    }
 } ], ( err ) => {
     if ( err ) throw err;
 
@@ -45,12 +29,50 @@ Server.register( [ {
 
             // Add handler
             route.handler = handler( r );
+
             // Don't parse
             if ( !route.config ) route.config = {};
             if ( route.method.toUpperCase() != 'GET' && route.method.toUpperCase() != 'HEAD' )
                 route.config.payload = {
                     parse: false
                 };
+
+            // Cleanup payload validation
+            if ( route.config.validate && route.config.validate.payload ) {
+
+                if ( !route.config.plugins )
+                    route.config.plugins = {};
+                if ( !route.config.plugins[ 'payload' ] )
+                // route.config.plugins[ 'payload' ] = route.config.validate.payload
+                    route.config.plugins[ 'payload' ] = {
+                    version: Joi.number()
+                        .default( 1 ),
+
+                    device: Joi.object( {
+                            id: Joi.string()
+                                .required()
+                        } )
+                        .required(),
+
+                    timestamp: Joi.date()
+                        .default( Date.now, 'Date.now()' ),
+
+                    measurements: Joi.array()
+                        .single()
+                        .default( [] )
+                        .items( Joi.object( {
+                            type: Joi.string()
+                                .required(),
+                            id: Joi.string(),
+                            value: Joi.any()
+                                .required(),
+                            unit: Joi.string()
+                        } ) )
+                }
+
+                route.config.validate.payload = undefined;
+
+            }
 
             // Register to HAPI
             Server.route( route );
@@ -74,6 +96,10 @@ Server.register( [ {
 
         // Return the real handler
         return function ( req, reply ) {
+
+            if ( routes[ route ] )
+                reply()
+                .status( 503 );
 
             // Round robin'
             let node = routes[ route ].pop();
@@ -102,15 +128,164 @@ Server.register( [ {
 
     function healthCheck( node ) {
 
+        // TODO healthCheck before cleaning
+
         Log.warn( 'Cleaned node', node );
-        Object.keys(routes).forEach( ( route ) => {
-            let i = routes[route].indexOf( node );
-            if ( ~i ) routes[route].splice( i, 1 );
-        } );
+        Object.keys( routes )
+            .forEach( ( route ) => {
+                let i = routes[ route ].indexOf( node );
+                if ( ~i ) routes[ route ].splice( i, 1 );
+            } );
         Events.emit( 'web:init' );
 
     }
 
+    Server.route( {
+        method: 'get',
+        path: '/api',
+        config: {
+            description: 'API Documentation'
+        },
+        handler( request, reply ) {
+
+            let base = request.headers.host || 'api';
+            let routes = Server.table()[ 0 ].table;
+
+            // Root Postman JSON object
+            let doc = {
+                id: base,
+                name: base,
+                description: JSON.stringify( Server.table()[ 0 ].info ),
+                timestamp: Date.now(),
+                owner: 'Poetry',
+                order: [],
+                remoteLink: "http://" + base + "/api",
+                public: true,
+
+                folders: [],
+                requests: []
+
+            };
+
+            let folders = {};
+
+            // Parse routes
+            routes.forEach( ( route ) => {
+
+                // Main properties
+                let request = {
+                    id: route.fingerprint,
+                    name: route.settings.description || route.fingerprint,
+                    description: ( route.settings.notes || [] )
+                        .join( '\n\n' ),
+                    collectionId: base,
+                    method: route.method,
+                    url: 'http://' + base + route.path,
+                    dataMode: 'raw',
+                    rawModeData: '',
+                    headers: "Content-Type: application/javascript\n"
+                }
+
+                if ( route.settings.validate.params ) {
+                    let params = Joi.object( route.settings.validate.params );
+                    request.description += '\n\n \n\n**URL Params**\n';
+                    request.description += J2M.convertSchema( params )
+                        .md;
+                }
+
+                if ( route.settings.validate.query ) {
+                    let query = Joi.object( route.settings.validate.query );
+                    request.description += '\n\n \n\n**URL Variables**\n';
+                    request.description += J2M.convertSchema( query )
+                        .md;
+                }
+
+                if ( route.settings.plugins.payload ) {
+                    let payload = Joi.object( route.settings.plugins.payload );
+                    payload = J2M.convertSchema( payload );
+                    request.description += '\n\n \n\n**Payload**\n';
+                    request.description += payload.md;
+                    var example = {};
+                    if ( payload.records && payload.records.length )
+                        payload.records.forEach( ( record ) => {
+
+                            if (!record.path) return;
+
+                            let setter = new Function( "example", "newval", "example." + record.path + " = newval; return example" );
+
+                            if ( record.examples && record.examples.length )
+                                return setter( example, record.examples[ 0 ] );
+
+                            switch ( record.type ) {
+                            case 'string':
+                                example = setter( example, "" );
+                                break;
+                            case 'number':
+                                example = setter( example, 0 );
+                                break;
+                            case 'array':
+                                example = setter( example, [] );
+                                break;
+                            case 'object':
+                                example = setter( example, {} );
+                                break;
+                            default:
+                                example = setter( example, null );
+                            }
+                        } );
+
+                    request.rawModeData = JSON.stringify( example, null, 4 );
+                }
+
+                // If no tag, root
+                if ( !route.settings.tags || !route.settings.tags.length )
+                    return doc.requests.push( request );
+
+                // For each tag, add it
+                route.settings.tags.forEach( ( tag ) => {
+                    let r = {};
+                    Object.keys( request )
+                        .forEach( ( key ) => {
+                            r[ key ] = request[ key ];
+                        } );
+                    r.folder = tag;
+                    r.id += '#' + tag;
+                    doc.requests.push( r );
+
+                    // Create folder if new
+                    if ( !folders[ tag ] )
+                        folders[ tag ] = {
+                            id: tag,
+                            name: tag,
+                            description: tag,
+                            owner: 'Poetry',
+                            order: [],
+                            collectionId: base
+                        };
+                    // Add in folder
+                    folders[ tag ].order.push( r.id );
+
+                } );
+
+            } );
+
+            Object.keys( folders )
+                .forEach( ( folder ) => {
+                    doc.folders.push( folders[ folder ] );
+                } );
+
+            reply( doc );
+
+        }
+    } );
+
     Events.emit( 'web:init' );
 
 } );
+
+Object.resolve = function ( path, obj, safe ) {
+    return path.split( '.' )
+        .reduce( function ( prev, curr ) {
+            return !safe ? prev[ curr ] : ( prev ? prev[ curr ] : undefined )
+        }, obj || self )
+}
